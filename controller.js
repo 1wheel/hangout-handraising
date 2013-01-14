d6fb64f7d508238fef3d5cc9bc8d7f3d81ad49a7
Adam Pearce <1wheel@gmail.com>
2013-01-14 15:33:59 -0600

    added controller.js

---
+++ controller.js
@@ -0,0 +1,15 @@
+var _participants;		//people currently running an instance of the hangout
+
+gapi.hangout.onApiReady.add(function(eventObj){
+	if (eventObj.isApiReady) {
+		_participants = gapi.hangout.enabledParicipants;
+		printParticipants();
+	}
+}
+
+function printParticipants(){
+	for (var i = 0; i < _participants.length; i++){
+		members = members + _participants[i].displayName;
+	}
+	document.getElementById('particpants').innerHTML = members;
+}
